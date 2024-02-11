5074546a9c5d62b62ad234a5e50c51a97bdf7c43
Arshad <arshadrauff008@gmail.com>
2024-02-11 22:52:26 +0530

    init project

---
+++ src/main/java/com/abc/lab/abclabsbe/AbcLabsBeApplication.java
@@ -0,0 +1,13 @@
+package com.abc.lab.abclabsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcLabsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcLabsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/lab/abclabsbe/AbcLabsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.lab.abclabsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcLabsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
